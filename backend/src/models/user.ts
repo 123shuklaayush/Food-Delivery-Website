231627387625f563018df6a9ffa84624b36eeb96
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-18 20:56:02 +0530

    added user test

---
+++ backend/src/models/user.ts
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  auth0Id: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  name: {
+    type: String,
+  },
+  addressLine1: {
+    type: String,
+  },
+  city: {
+    type: String,
+  },
+  country: {
+    type: String,
+  },
+});
+export const User = mongoose.model("User", userSchema);
