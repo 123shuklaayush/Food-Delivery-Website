818916b1f7491d55dc43d75aaebe6d6fd93a45ea
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-19 12:11:45 +0530

    added react-query for user authentication

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,3 +1,4 @@
+import { useCreateMyUser } from "@/api/MyUserApi";
 import { AppState, Auth0Provider, User } from "@auth0/auth0-react";
 
 type Props = {
@@ -5,6 +6,7 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const { createUser } = useCreateMyUser();
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
@@ -13,7 +15,9 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
     throw new Error("Unable to Initialise Auth");
   }
   const onRedirectCallback = (appState?: AppState, user?: User) => {
-    console.log("User", user);
+    if (user?.sub && user.email) {
+      createUser({ auth0Id: user.sub, email: user.email });
+    }
   };
   return (
     <Auth0Provider
