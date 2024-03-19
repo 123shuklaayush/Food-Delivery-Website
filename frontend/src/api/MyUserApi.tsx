b20b7e3dba5e1fb373208693a5dc33fa0b2091c8
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-20 01:25:26 +0530

    added toast notification

--- frontend/src/api/MyUserApi.tsx
+++ frontend/src/api/MyUserApi.tsx
@@ -1,5 +1,6 @@
 import { useAuth0 } from "@auth0/auth0-react";
 import { useMutation } from "react-query";
+import { toast } from "sonner";
 
 const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
@@ -61,9 +62,19 @@ export const useUpdateMyUser = () => {
   const {
     mutateAsync: updateUser,
     isLoading,
+    error,
     isError,
     isSuccess,
+    reset
   } = useMutation(updateMyUserRequest);
 
-  return { updateUser, isLoading}
+  if (isSuccess) {
+    toast.success("User Profile Updated");
+  }
+
+  if (error) {
+    toast.error(error.toString());
+    reset();
+  }
+  return { updateUser, isLoading };
 };
