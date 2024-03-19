a8736fdb47450a284fc612cf8ff073471149712f
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-20 01:15:32 +0530

    updated user profile request

--- frontend/src/pages/UserProfilePage.tsx
+++ frontend/src/pages/UserProfilePage.tsx
@@ -1,6 +1,8 @@
+import { useUpdateMyUser } from "@/api/MyUserApi";
 import UserProfileForm from "@/forms/user-profile-form/UserProfileForm";
 const UserProfilePage = () => {
-  return <UserProfileForm />;
+  const { updateUser, isLoading } = useUpdateMyUser();
+  return <UserProfileForm onSave={updateUser} isLoading ={isLoading}/>;
 };
 
 export default UserProfilePage;
