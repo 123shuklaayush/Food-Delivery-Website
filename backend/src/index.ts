1b254aedfa2fb04370b811f6febc532585cd8d8b
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-20 12:03:39 +0530

    added code for server  deployment

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -16,6 +16,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async(req: Request, res: Response) => {
+  res.send("Health OK");
+});
 app.use("/api/my/user", myUserRoute);
 app.listen(7000, () => {
   console.log("Server is running on port 7000");
