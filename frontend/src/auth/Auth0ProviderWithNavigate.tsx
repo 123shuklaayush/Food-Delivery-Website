842cccab89569cbfc00fba92d777f66fa9ac7494
Ayush Shukla <123shuklaayush@gmail.com>
2024-03-20 18:31:08 +0530

    fixed refresh bug

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -1,4 +1,4 @@
-import { Auth0Provider } from "@auth0/auth0-react";
+import { AppState, Auth0Provider } from "@auth0/auth0-react";
 import { useNavigate } from "react-router-dom";
 
 type Props = {
@@ -6,18 +6,21 @@ type Props = {
 };
 
 const Auth0ProviderWithNavigate = ({ children }: Props) => {
+  const navigate = useNavigate();
+
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
   const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
-  const navigate = useNavigate();
 
   if (!domain || !clientId || !redirectUri || !audience) {
-    throw new Error("Unable to Initialise Auth");
+    throw new Error("unable to initialise auth");
   }
-  const onRedirectCallback = () => {
-    navigate("/auth-callback");
+
+  const onRedirectCallback = (appState?: AppState) => {
+    navigate(appState?.returnTo || "/auth-callback");
   };
+
   return (
     <Auth0Provider
       domain={domain}
